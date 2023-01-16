906c9db6bf82e43808313624a916c47bdb113f02
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-16 14:29:33 -0600

    Add custom constraint

---
+++ RoutingExample/Custom Constraints/MonthsCustomConstraint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions; 
+
+namespace RoutingExample.CustomConstraints
+{
+	public class MonthsCustomConstraint : IRouteConstraint
+	{
+		public MonthsCustomConstraint()
+		{
+		}
+
+        /// <summary>
+        /// Checks for a match on the route endpoint. If match found, then return true, else return false
+        /// </summary>
+        /// <param name="httpContext">The context received in middleware. Provides request and response objects</param>
+        /// <param name="route">Represents an object that represents the route in which this constraint is applied</param>
+        /// <param name="routeKey">The parameter in the query string</param>
+        /// <param name="values">The values received in the query string in the route</param>
+        /// <param name="routeDirection">1) Checks whether this constraint matches with incoming request or 2) generate a URL based on values supplied</param>
+        /// <returns>True / false value for if a match is made</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (!values.ContainsKey(routeKey))
+            {
+                return false; // no key provided
+            }
+
+            Regex regex = new Regex("^(apr|jul|oct|jan)$");
+            string? month = Convert.ToString(values[routeKey]);
+
+            if (regex.IsMatch(month))
+            {
+                return true; // found match
+            }
+
+            return false; // no match found
+        }
+    }
+}
+
--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -1,18 +1,14 @@
 ﻿using System.Net;
-using Microsoft.AspNetCore.Http; 
+using Microsoft.AspNetCore.Http;
+using RoutingExample.CustomConstraints; 
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.Use(async (context, next) =>
+builder.Services.AddRouting(options =>
 {
-    Endpoint endpoint = context.GetEndpoint();
-    if (endpoint != null)
-    {
-        await context.Response.WriteAsync($"Endpoint is {endpoint.DisplayName}\n");
-    }
-    await next(context);
-}); 
+    // Register MonthsCustomConstraint custom constraint in the Services 
+    options.ConstraintMap.Add("months", typeof(MonthsCustomConstraint));
+});
+var app = builder.Build();
 
 // Enable routing
 app.UseRouting();
@@ -42,16 +38,60 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync($"File name is {fileName}, extension is {extension}");
     });
 
-    endpoints.Map("employee/profile/{EmployeeName}", async context =>
+    endpoints.Map("employee/profile/{EmployeeName:length(3,7):alpha=none}", async context =>
     {
-        string? employeeName = Convert.ToString(context.Request.RouteValues["EmployeeName"]);
-        await context.Response.WriteAsync($"Employee name is {employeeName}");
+        if (context.Request.RouteValues.ContainsKey("EmployeeName"))
+        {
+            string? employeeName = Convert.ToString(context.Request.RouteValues["EmployeeName"]);
+            await context.Response.WriteAsync($"Employee name is {employeeName}");
+        }
+        else
+        {
+            await context.Response.WriteAsync($"Employee name is not provided");
+        }
+    });
+
+    endpoints.Map("product/{id:int:range(1,100)?}", async context =>
+    {
+        if (context.Request.RouteValues.ContainsKey("id"))
+        {
+            int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            await context.Response.WriteAsync($"Product ID is {id}");
+        }
+        else
+        {
+            await context.Response.WriteAsync($"Product ID is not provided");
+        }
+    });
+
+    endpoints.Map("daily-digest-report/{reportDate:datetime}", async context =>
+    {
+        var reportDate = Convert.ToDateTime(context.Request.RouteValues["reportDate"]);
+        await context.Response.WriteAsync($"Report date is {reportDate.ToShortDateString()}");
+    });
+
+    endpoints.Map("sales-report/{year:int:min(1900)}/{month:months}", async context =>
+    {
+        int year = Convert.ToInt32(context.Request.RouteValues["year"]);
+        string? month = Convert.ToString(context.Request.RouteValues["month"]);
+        await context.Response.WriteAsync($"Sales report as of {month} {year}");
+    });
+
+    endpoints.Map("sales-report/2023/may", async context =>
+    {
+        await context.Response.WriteAsync($"Sales report as of May 2023");
+    });
+
+    endpoints.Map("cities/{cityID:guid}", async context =>
+    {
+        var cityID = Guid.Parse(Convert.ToString(context.Request.RouteValues["cityID"]));
+        await context.Response.WriteAsync($"City ID is {cityID}");
     });
 });
 
 app.Run(async context =>
 {
-    await context.Response.WriteAsync($"Request received - {context.Request.Path}");
+    await context.Response.WriteAsync($"{context.Request.Path} does not exist.");
 }); 
 
 app.Run();
