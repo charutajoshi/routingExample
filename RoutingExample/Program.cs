7d221f4a4dd50f0423f8aef1be20bde597ad96f3
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-16 00:10:19 -0600

    Initial commit

---
+++ RoutingExample/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("test1", async (context) =>
+    {
+        await context.Response.WriteAsync("hello1"); 
+    });
+
+    endpoints.MapPost("test2", async (context) =>
+    {
+        await context.Response.WriteAsync("hello2");
+    });
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request received - {context.Request.Path}");
+}); 
+
+app.Run();
+
