7c89abb176c6f53d4105a377050fe884226c4e9a
charutaj <charutajoshi@Charutas-MacBook-Air.local>
2023-01-16 10:21:30 -0600

    Add GetEndPoint() method

--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -1,8 +1,35 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Http; 
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Endpoint endpoint = context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint is {endpoint.DisplayName}\n");
+    }
+    await next(context);
+}); 
+
+// Enable routing
 app.UseRouting();
 
+app.Use(async (context, next) =>
+{
+    // Endpoint? = nullable reference type
+    Endpoint? endpoint = context.GetEndpoint();
+
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint is {endpoint.DisplayName}\n");
+    }
+
+    await next(context);
+});
+
+// Create endpoints
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGet("test1", async (context) =>
